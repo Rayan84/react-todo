7017e8ed8703eabf216edfb6e7f1a01d7624a39a
Rayan84 <rayan_84@live.com>
2021-10-27 11:00:15 +0200

    Add component files

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,11 @@
+import React from "react"
+class TodoContainer extends React.component {
+    render() {
+      return (
+          <div>
+              <h1>Hellom from Create React App</h1>
+              <p>I am in a React Component</p>
+          </div>
+      )
+    }
+}
